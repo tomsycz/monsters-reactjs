0215d05d355f47e6d3a85f82cbff93af5a70e0d9
Thomas Syczyk <tomsycz@gmail.com>
2021-04-22 11:35:26 +0100

    cors

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
-    // this.handleChange = this.handleChange.bind(this);
   }
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
